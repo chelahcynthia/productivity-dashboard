2eb105a69c574ade775a7eca3683801609c735f9
chelahcynthia <cchelangat989@gmail.com>
2023-05-16 12:00:37 +0300

    Imports all the components on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
+// import all the components here
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Body from "./Components/Body/Body";
+import Account from "./Components/Accounts/Account"
+
 function App() {
-  return <div>App</div>;
+  return <div className="container">
+  <Sidebar />
+  <Body />
+  <Account />
+
+    </div>;
 }
 
 export default App;
