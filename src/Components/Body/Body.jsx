500053fd183577007c477997776ee3aaa8b03d83
chelahcynthia <cchelangat989@gmail.com>
2023-05-17 10:15:38 +0300

    Creates an unordered list on sidebar.jsx

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-
 import "./Body.css";
+
 import Sidebar from "../Sidebar/Sidebar";
+import Navbar from "../Navbar/Navbar";
 
 function Body() {
   return (
     <div>
-      <div>
-
-      <Sidebar />
+      <div className="mainHeaderContainer">
+        <Sidebar />
+        <Navbar />
       </div>
     </div>
   );
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import './Sidebar.css';
+import React from "react";
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div>
-    Sidebar
+    <div className="sidebar">
+      <div>
+        <p style={{ textAlign: "start" }}>Dashboard</p>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Sales</a>
+          </li>
+          <li>
+            <a href="">Analytics</a>
+          </li>
+        </ul>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
